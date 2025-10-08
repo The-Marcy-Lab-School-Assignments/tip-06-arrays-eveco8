2dc99af8b3c02fcd1db4d0ae8b192eb03c329ad3
Evelin Cobos <evecobos8@gmail.com>
2025-10-08 11:38:33 -0400

    not correct

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 //write your code here
+const getAverage = (numbers) => {
+    let total = 0
+    for (i = 0; i < numbers.length; i++) {
+        total += numbers[i]
+    }
+    return total / numbers.length;
+}
+
+
+const findMax = (numbers) => {
+    let firstNum = numbers[0]
+    let largestNum;
+    for (i = 0; i < numbers.length; i++) {
+        if (firstNum > numbers[i]) {
+            firstNum = numbers[i]
+        } else {
+            largestNum = numbers[i]
+        }
+    }
+    console.log(firstNum)
+}
+findMax([3, 7, 2, 9, 5])
+findMax([-1, -5, -3])
+
+
+/*.  const findMax = (numbers) => {
+        let max = numbers[0]
+        for(i = 0; i < numbers.length; i++) {
+            if(max > numbers[i]) {
+            max = numbers[i]
+        }
+            return max
+    }
+
+*/
